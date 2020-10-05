5c45b19d2895e6c42cb5b24b3d2e155c7d0bfb56
sylvain <sylvain.ducamp@gammadia.ch>
2020-10-05 15:14:35 +0200

    [FRM-6764] Effective: Generate resume for a day
    - Fix stan reports
    - Add tests

--- vendor-unmanaged/gammadia/date-time-extra/src/Helper/DurationHelper.php
+++ vendor-unmanaged/gammadia/date-time-extra/src/Helper/DurationHelper.php
@@ -13,17 +13,18 @@ final class DurationHelper
 {
     public static function percentage(Duration $a, Duration $b): Percentage
     {
-        return Percentage::ofRatio($a->toNanos(), $b->toNanos());
+        return Percentage::ofRatio($a->toMillis(), $b->toMillis());
     }
 
     public static function hoursToDuration(float $hours): Duration
     {
-        return Duration::ofNanos((int) ($hours * LocalTime::SECONDS_PER_HOUR * LocalTime::NANOS_PER_SECOND));
+        return Duration::ofMillis((int)round($hours * LocalTime::SECONDS_PER_HOUR * LocalTime::MILLIS_PER_SECOND,
+            PHP_ROUND_HALF_EVEN));
     }
 
     public static function applyPercentage(Duration $duration, Percentage $rate): Duration
     {
-        return Duration::ofNanos((int) ($duration->toNanos() * $rate->factor()));
+        return Duration::ofMillis((int) round($duration->toMillis() * $rate->factor(), PHP_ROUND_HALF_EVEN));
     }
 
     public static function applyDailyPercentage(Duration $duration, LocalDateTimeInterval $timeRange): Duration
