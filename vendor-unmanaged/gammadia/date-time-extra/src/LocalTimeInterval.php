6d1f54ff66df3ed7bf1fba04c3079ef1b70682f7
Dorian Villet <gnutix@gmail.com>
2020-12-30 12:58:45 +0100

    [FRM-6505] DateTimeExtra: basic implementation of LocalTimeInterval.

---
+++ vendor-unmanaged/gammadia/date-time-extra/src/LocalTimeInterval.php
@@ -0,0 +1,232 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gammadia\DateTimeExtra;
+
+use Brick\DateTime\Duration;
+use Brick\DateTime\LocalDate;
+use Brick\DateTime\LocalTime;
+use Throwable;
+use Webmozart\Assert\Assert;
+
+final class LocalTimeInterval
+{
+    /**
+     * @var string
+     */
+    private const SEPARATOR = '/';
+
+    /**
+     * @var string
+     */
+    private const INFINITY = '-';
+
+    /**
+     * @var int
+     */
+    private const FINITE = 0;
+
+    /**
+     * @var int
+     */
+    private const INFINITE_START = 1;
+
+    /**
+     * @var int
+     */
+    private const INFINITE_END = 2;
+
+    /**
+     * @var LocalTime
+     */
+    private $timepoint;
+
+    /**
+     * @var Duration|null
+     */
+    private $duration;
+
+    /**
+     * @var int
+     */
+    private $finitude;
+
+    /**
+     * @var LocalDate
+     */
+    private static $arbitraryDateCache;
+
+    private function __construct(LocalTime $timepoint, ?Duration $duration = null, int $finitude = self::FINITE)
+    {
+        $this->timepoint = $timepoint;
+        $this->duration = $duration;
+        $this->finitude = $finitude;
+    }
+
+    public function __toString(): string
+    {
+        return $this->toString();
+    }
+
+    public static function for(LocalTime $timepoint, Duration $duration): self
+    {
+        return new self($timepoint, $duration);
+    }
+
+    public static function from(LocalDateTimeInterval $timeRange): self
+    {
+        Assert::false($timeRange->hasInfiniteStart() && $timeRange->hasInfiniteEnd(), 'A timepoint is mandatory.');
+
+        $timepoint = (!$timeRange->hasInfiniteStart() ? $timeRange->getFiniteStart() : $timeRange->getFiniteEnd())
+            ->getTime();
+        $duration = $timeRange->isFinite() ? $timeRange->getDuration() : null;
+        $finitude = $timeRange->isFinite()
+            ? self::FINITE
+            : ($timeRange->hasInfiniteStart() ? self::INFINITE_START : self::INFINITE_END);
+
+        return new self($timepoint, $duration, $finitude);
+    }
+
+    public static function between(?LocalTime $startTime, ?LocalTime $endTime): self
+    {
+        Assert::false(null === $startTime && null === $endTime, 'A timepoint is mandatory.');
+
+        // For finite range, we calculate the duration between the two hour ranges (including overnight scenarios)
+        if (null !== $startTime && null !== $endTime) {
+            $start = self::arbitraryDate()->atTime($startTime);
+            $end = self::arbitraryDate()->atTime($endTime);
+            if ($end->isBefore($start)) {
+                $end = $end->plusDays(1);
+            }
+
+            $timepoint = $startTime;
+            $duration = LocalDateTimeInterval::between($start, $end)->getDuration();
+            $finitude = self::FINITE;
+        } else {
+            $timepoint = $endTime ?? $startTime;
+            Assert::notNull($timepoint);
+            $duration = null;
+            $finitude = null === $endTime ? self::INFINITE_END : self::INFINITE_START;
+        }
+
+        return new self($timepoint, $duration, $finitude);
+    }
+
+    public static function empty(LocalTime $timepoint): self
+    {
+        return new self($timepoint, Duration::zero());
+    }
+
+    public static function until(LocalTime $timepoint): self
+    {
+        return new self($timepoint, null, self::INFINITE_START);
+    }
+
+    public static function since(LocalTime $timepoint): self
+    {
+        return new self($timepoint, null, self::INFINITE_END);
+    }
+
+    public static function day(): self
+    {
+        return new self(LocalTime::min(), Duration::ofDays(1));
+    }
+
+    public function toString(): string
+    {
+        if ($this->hasInfiniteStart()) {
+            $arguments = [self::INFINITY, $this->timepoint];
+        } elseif ($this->hasInfiniteEnd()) {
+            $arguments = [$this->timepoint, self::INFINITY];
+        } else {
+            $arguments = [$this->timepoint, $this->duration];
+        }
+
+        return implode(self::SEPARATOR, $arguments);
+    }
+
+    /**
+     * @param string $textToParse A LocalTime + optional Duration (12:34/PT2H, PT2H/12:34, -/12:34 or 12:34/-)
+     */
+    public static function parse(string $textToParse): self
+    {
+        try {
+            Assert::contains($textToParse, self::SEPARATOR);
+            [$firstPart, $secondPart] = explode(self::SEPARATOR, $textToParse);
+            Assert::false(self::INFINITY === $firstPart && self::INFINITY === $secondPart, 'A timepoint is mandatory.');
+
+            if (self::INFINITY === $firstPart) {
+                $timepoint = LocalTime::parse($secondPart);
+                $duration = null;
+                $finitude = self::INFINITE_START;
+            } elseif (self::INFINITY === $secondPart) {
+                $timepoint = LocalTime::parse($firstPart);
+                $duration = null;
+                $finitude = self::INFINITE_END;
+            } else {
+                // Let's allow for reversed arguments, because we can and it doesn't matter
+                try {
+                    $timepoint = LocalTime::parse($firstPart);
+                    $duration = Duration::parse($secondPart);
+                } catch (\Throwable $throwable) {
+                    $timepoint = LocalTime::parse($secondPart);
+                    $duration = Duration::parse($firstPart);
+                }
+                $finitude = self::FINITE;
+            }
+
+            return new self($timepoint, $duration, $finitude);
+        } catch (Throwable $throwable) {
+            throw IntervalParseException::localTimeInterval($textToParse, $throwable);
+        }
+    }
+
+    public function atDate(LocalDate $date): LocalDateTimeInterval
+    {
+        $localDateTime = $this->timepoint->atDate($date);
+        $start = $end = null;
+
+        if (!$this->hasInfiniteStart()) {
+            $start = null !== $this->duration && $this->duration->isNegative()
+                ? $localDateTime->plusDuration($this->duration)
+                : $localDateTime;
+        }
+        if (!$this->hasInfiniteEnd()) {
+            $end = null !== $this->duration && $this->duration->isPositive()
+                ? $localDateTime->plusDuration($this->duration)
+                : $localDateTime;
+        }
+
+        return LocalDateTimeInterval::between($start, $end);
+    }
+
+    public function isEmpty(): bool
+    {
+        return null !== $this->duration && $this->duration->isZero();
+    }
+
+    public function isFinite(): bool
+    {
+        return self::FINITE === $this->finitude;
+    }
+
+    public function hasInfiniteStart(): bool
+    {
+        return self::INFINITE_START === $this->finitude;
+    }
+
+    public function hasInfiniteEnd(): bool
+    {
+        return self::INFINITE_END === $this->finitude;
+    }
+
+    private static function arbitraryDate(): LocalDate
+    {
+        if (null === self::$arbitraryDateCache) {
+            self::$arbitraryDateCache = LocalDate::parse('2020-01-02');
+        }
+
+        return self::$arbitraryDateCache;
+    }
+}
---
+++ vendor-unmanaged/gammadia/date-time-extra/tests/LocalTimeIntervalTest.php
@@ -0,0 +1,257 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gammadia\DateTimeExtra\Test\Unit;
+
+use Brick\DateTime\Duration;
+use Brick\DateTime\LocalDate;
+use Brick\DateTime\LocalTime;
+use Gammadia\DateTimeExtra\IntervalParseException;
+use Gammadia\DateTimeExtra\LocalDateTimeInterval;
+use Gammadia\DateTimeExtra\LocalTimeInterval;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+final class LocalTimeIntervalTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    private const DATE = '2020-01-02';
+
+    /**
+     * @return iterable<mixed>
+     */
+    public function emptyISO(): iterable
+    {
+        $empty = '2020-01-02T00:00/2020-01-02T00:00';
+        yield ['00:00/PT-0M', $empty];
+        yield ['00:00/PT0M', $empty];
+        yield ['00:00:00/PT0S', $empty];
+
+        $empty = '2020-01-02T12:00/2020-01-02T12:00';
+        yield ['12:00/PT-0M', $empty];
+        yield ['12:00/PT0M', $empty];
+        yield ['12:00:00/PT0S', $empty];
+
+        $empty = '2020-01-02T23:59/2020-01-02T23:59';
+        yield ['23:59/PT-0M', $empty];
+        yield ['23:59/PT0M', $empty];
+        yield ['23:59:59/PT0S', '2020-01-02T23:59:59/2020-01-02T23:59:59'];
+
+        yield 'Reversed arguments work too' => ['PT0S/08:00'];
+    }
+
+    /**
+     * @return iterable<mixed>
+     */
+    public function validISO(): iterable
+    {
+        // Positive intervals at different times
+        yield ['00:00/PT12H', '2020-01-02T00:00/2020-01-02T12:00'];
+        yield ['00:00/PT24H', '2020-01-02T00:00/2020-01-03T00:00'];
+        yield ['00:00/PT48H', '2020-01-02T00:00/2020-01-04T00:00'];
+        yield ['00:00/PT8H24M37S', '2020-01-02T00:00/2020-01-02T08:24:37'];
+        yield ['12:00/PT12H', '2020-01-02T12:00/2020-01-03T00:00'];
+        yield ['12:00/PT24H', '2020-01-02T12:00/2020-01-03T12:00'];
+        yield ['12:00/PT48H', '2020-01-02T12:00/2020-01-04T12:00'];
+        yield ['12:00/PT8H24M37S', '2020-01-02T12:00/2020-01-02T20:24:37'];
+        yield ['23:59/PT12H', '2020-01-02T23:59/2020-01-03T11:59'];
+        yield ['23:59/PT24H', '2020-01-02T23:59/2020-01-03T23:59'];
+        yield ['23:59/PT48H', '2020-01-02T23:59/2020-01-04T23:59'];
+        yield ['23:59/PT8H24M37S', '2020-01-02T23:59/2020-01-03T08:23:37'];
+
+        // Negative intervals
+        yield ['00:00/PT-12H', '2020-01-01T12:00/2020-01-02T00:00'];
+        yield ['00:00/PT-24H', '2020-01-01T00:00/2020-01-02T00:00'];
+        yield ['00:00/PT-48H', '2019-12-31T00:00/2020-01-02T00:00'];
+        yield ['00:00/PT-8H-24M-37S', '2020-01-01T15:35:23/2020-01-02T00:00'];
+        yield ['12:00/PT-12H', '2020-01-02T00:00/2020-01-02T12:00'];
+        yield ['12:00/PT-24H', '2020-01-01T12:00/2020-01-02T12:00'];
+        yield ['12:00/PT-48H', '2019-12-31T12:00/2020-01-02T12:00'];
+        yield ['12:00/PT-8H-24M-37S', '2020-01-02T03:35:23/2020-01-02T12:00'];
+        yield ['23:59/PT-12H', '2020-01-02T11:59/2020-01-02T23:59'];
+        yield ['23:59/PT-24H', '2020-01-01T23:59/2020-01-02T23:59'];
+        yield ['23:59/PT-48H', '2019-12-31T23:59/2020-01-02T23:59'];
+        yield ['23:59/PT-8H-24M-37S', '2020-01-02T15:34:23/2020-01-02T23:59'];
+
+        // Mixed intervals (negative and positives)
+        yield 'Negative overall duration with positive internals' => [
+            '00:00/PT-8H24M-37S',
+            '2020-01-01T16:23:23/2020-01-02T00:00',
+        ];
+        yield 'Positive overall duration with negative internals' => [
+            '00:00/PT8H-24M37S',
+            '2020-01-02T00:00/2020-01-02T07:36:37',
+        ];
+
+        // Infinite start or end
+        yield ['-/00:00', '-/2020-01-02T00:00'];
+        yield ['00:00/-', '2020-01-02T00:00/-'];
+
+        // Misc
+        yield 'Reversed arguments work too' => ['PT2H/08:00', '2020-01-02T08:00/2020-01-02T10:00'];
+        yield '3 year (365 * 3) interval ends up one day sooner because 2020 is a leap year with 366 days' => [
+            '00:00/P1095D',
+            '2020-01-02T00:00/2023-01-01T00:00',
+        ];
+    }
+
+    /**
+     * @return iterable<mixed>
+     */
+    public function invalidISO(): iterable
+    {
+        yield 'An infinite time interval makes no sense, there must be a timepoint.' => ['-/-'];
+        yield '24:00 format is not supported' => ['24:00-PT0S'];
+        yield 'Two LocalTimes are not supported, use ::between() instead.' => ['08:00/12:00'];
+        yield 'Bad separator' => ['08:00-PT12H'];
+    }
+
+    /**
+     * @return iterable<mixed>
+     */
+    public function validTimeRanges(): iterable
+    {
+        $date = LocalDate::parse(self::DATE);
+
+        yield [LocalDateTimeInterval::day($date), '00:00/PT24H'];
+        yield [LocalDateTimeInterval::since($date->atTime(LocalTime::min())), '00:00/-'];
+        yield [LocalDateTimeInterval::until($date->atTime(LocalTime::min())), '-/00:00'];
+    }
+
+    /**
+     * @return iterable<mixed>
+     */
+    public function invalidTimeRanges(): iterable
+    {
+        yield [LocalDateTimeInterval::forever()];
+    }
+
+    /**
+     * @dataProvider emptyISO
+     * @dataProvider validISO
+     */
+    public function testParse(string $iso): void
+    {
+        LocalTimeInterval::parse($iso);
+        $this->addToAssertionCount(1);
+    }
+
+    /**
+     * @dataProvider invalidISO
+     */
+    public function testParseInvalid(string $iso): void
+    {
+        $this->expectException(IntervalParseException::class);
+        LocalTimeInterval::parse($iso);
+    }
+
+    /**
+     * @dataProvider validISO
+     */
+    public function testAtDate(string $iso, string $expected): void
+    {
+        self::assertSame($expected, (string) LocalTimeInterval::parse($iso)->atDate(LocalDate::parse(self::DATE)));
+    }
+
+    /**
+     * @dataProvider emptyISO
+     */
+    public function testIsEmpty(string $iso): void
+    {
+        self::assertTrue(LocalTimeInterval::parse($iso)->isEmpty());
+    }
+
+    /**
+     * @dataProvider validISO
+     */
+    public function testIsNotEmpty(string $iso): void
+    {
+        self::assertFalse(LocalTimeInterval::parse($iso)->isEmpty());
+    }
+
+    public function testEmptyNamedConstructor(): void
+    {
+        self::assertSame(
+            (string) LocalTimeInterval::empty(LocalTime::parse('12:34')),
+            (string) LocalTimeInterval::for(LocalTime::parse('12:34'), Duration::zero())
+        );
+    }
+
+    public function testForNamedConstructor(): void
+    {
+        self::assertSame(
+            (string) LocalTimeInterval::parse('12:34/PT2H'),
+            (string) LocalTimeInterval::for(LocalTime::parse('12:34'), Duration::ofHours(2))
+        );
+    }
+
+    public function testDayNamedConstructor(): void
+    {
+        self::assertSame((string) LocalTimeInterval::parse('00:00/P1D'), (string) LocalTimeInterval::day());
+    }
+
+    /**
+     * @dataProvider validTimeRanges
+     */
+    public function testFromNamedConstructor(LocalDateTimeInterval $timeRange, string $expected): void
+    {
+        $localTimeInterval = LocalTimeInterval::from($timeRange);
+        self::assertSame($expected, (string) $localTimeInterval);
+
+        // Reconstructing the timerange works if you give the exact same date
+        self::assertSame((string) $timeRange, (string) $localTimeInterval->atDate(LocalDate::parse(self::DATE)));
+        self::assertNotSame((string) $timeRange, (string) $localTimeInterval->atDate(LocalDate::parse('2020-01-01')));
+    }
+
+    /**
+     * @dataProvider invalidTimeRanges
+     */
+    public function testFromNamedConstructorDoesntSupportForeverRanges(LocalDateTimeInterval $timeRange): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        LocalTimeInterval::from($timeRange);
+    }
+
+    public function testToString(): void
+    {
+        $localTimeInterval = LocalTimeInterval::for(LocalTime::parse('12:34'), Duration::ofHours(2)->plusMinutes(30));
+
+        self::assertSame('12:00/-', (string) LocalTimeInterval::since(LocalTime::parse('12:00')));
+        self::assertSame('-/12:00', (string) LocalTimeInterval::until(LocalTime::parse('12:00')));
+
+        self::assertSame('12:34/PT2H30M', (string) $localTimeInterval);
+        self::assertSame((string) $localTimeInterval, $localTimeInterval->toString());
+    }
+
+    /**
+     * @dataProvider between
+     */
+    public function testBetween(string $expectedIso, ?string $startTimeIso, ?string $endTimeIso): void
+    {
+        $startTime = null !== $startTimeIso ? LocalTime::parse($startTimeIso) : null;
+        $endTime = null !== $endTimeIso ? LocalTime::parse($endTimeIso) : null;
+
+        self::assertSame($expectedIso, (string) LocalTimeInterval::between($startTime, $endTime));
+    }
+
+    /**
+     * @return iterable<mixed>
+     */
+    public function between(): iterable
+    {
+        yield 'Two midnights equals an empty duration starting at midnight.' => ['00:00/PT0S', '00:00', '00:00'];
+
+        yield ['00:00/PT12H', '00:00', '12:00'];
+        yield ['12:00/PT12H', '12:00', '00:00'];
+        yield ['00:00/PT1M', '00:00', '00:01'];
+        yield ['00:00/PT23H59M', '00:00', '23:59'];
+        yield ['18:00/PT14H', '18:00', '08:00'];
+
+        yield ['18:00/-', '18:00', null];
+        yield ['-/18:00', null, '18:00'];
+    }
+}
