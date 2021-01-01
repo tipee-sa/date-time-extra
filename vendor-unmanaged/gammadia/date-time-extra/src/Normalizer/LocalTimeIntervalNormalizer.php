f2d1c1a8f99e616ce614ca883f2654512d18aa59
Dorian Villet <gnutix@gmail.com>
2021-01-01 14:59:40 +0100

    [FRM-6505] DateTimeExtra: add normalizer for LocalTimeInterval.

---
+++ vendor-unmanaged/gammadia/date-time-extra/src/Normalizer/LocalTimeIntervalNormalizer.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gammadia\DateTimeExtra\Normalizer;
+
+use Gammadia\DateTimeExtra\LocalTimeInterval;
+use Symfony\Component\Serializer\Exception\NotNormalizableValueException;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Throwable;
+
+class LocalTimeIntervalNormalizer implements NormalizerInterface, DenormalizerInterface
+{
+    /**
+     * @param LocalTimeInterval $object
+     * @param array<mixed> $context
+     */
+    public function normalize($object, ?string $format = null, array $context = []): string
+    {
+        return $object->toString();
+    }
+
+    /**
+     * @param mixed $data
+     * @param class-string $type
+     * @param array<mixed> $context
+     */
+    public function denormalize($data, string $type, ?string $format = null, array $context = []): LocalTimeInterval
+    {
+        try {
+            return LocalTimeInterval::parse($data);
+        } catch (Throwable $e) {
+            throw new NotNormalizableValueException(sprintf('%s (%s)', $e->getMessage(), $type));
+        }
+    }
+
+    /**
+     * @param mixed $data
+     */
+    public function supportsNormalization($data, ?string $format = null): bool
+    {
+        return $data instanceof LocalTimeInterval;
+    }
+
+    /**
+     * @param mixed $data
+     * @param class-string $type
+     */
+    public function supportsDenormalization($data, string $type, ?string $format = null): bool
+    {
+        return LocalTimeInterval::class === $type;
+    }
+}
---
+++ vendor-unmanaged/gammadia/date-time-extra/tests/Normalizer/LocalTimeIntervalNormalizerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gammadia\DateTimeExtra\Test\Unit\Normalizer;
+
+use Brick\DateTime\Duration;
+use Brick\DateTime\LocalTime;
+use Gammadia\DateTimeExtra\LocalTimeInterval;
+use Gammadia\DateTimeExtra\Normalizer\LocalTimeIntervalNormalizer;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Serializer\Exception\NotNormalizableValueException;
+
+class LocalTimeIntervalNormalizerTest extends TestCase
+{
+    public function testNormalize(): void
+    {
+        $normalizer = new LocalTimeIntervalNormalizer();
+        $localTimeInterval = LocalTimeInterval::for(LocalTime::parse('12:34'), Duration::ofHours(2));
+
+        self::assertTrue($normalizer->supportsNormalization($localTimeInterval));
+        self::assertFalse($normalizer->supportsNormalization(LocalTime::parse('10:15')));
+        self::assertSame('12:34/PT2H', $normalizer->normalize($localTimeInterval));
+    }
+
+    public function testDenormalize(): void
+    {
+        $normalizer = new LocalTimeIntervalNormalizer();
+        $iso = '12:34/PT2H';
+        $localTimeInterval = LocalTimeInterval::for(LocalTime::parse('12:34'), Duration::ofHours(2));
+
+        self::assertTrue($normalizer->supportsDenormalization($iso, LocalTimeInterval::class));
+        self::assertFalse($normalizer->supportsDenormalization($iso, LocalTime::class));
+        self::assertSame((string) $localTimeInterval, (string) $normalizer->denormalize($iso, LocalTimeInterval::class));
+
+        $this->expectException(NotNormalizableValueException::class);
+        $normalizer->denormalize('08:00/16:00', LocalTimeInterval::class);
+    }
+}
